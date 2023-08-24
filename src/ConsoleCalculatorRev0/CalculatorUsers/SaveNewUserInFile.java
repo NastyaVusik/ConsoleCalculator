2b1dd1d6c259b25a7e3d91e443f8e10a8b1e7133
Anastasiya Vusik <nastyavusik13@gmail.com>
2023-08-24 14:35:06 +0300

    Class Session.java was created and modified

--- src/ConsoleCalculatorRev0/CalculatorUsers/SaveNewUserInFile.java
+++ src/ConsoleCalculatorRev0/CalculatorUsers/SaveNewUserInFile.java
@@ -8,8 +8,9 @@ import java.io.*;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-public class SaveNewUserInFile implements SaveNewUser{
+public class SaveNewUserInFile implements SaveNewUser {
 
     //Create object of class ConsoleWriter
     private final ConsoleWriter consoleWriter = new ConsoleWriter();
@@ -21,24 +22,27 @@ public class SaveNewUserInFile implements SaveNewUser{
     Integer lastID = 0;
 
 
-    public SaveNewUserInFile (){
-       lastID = getUsersInfoArrayList().size();
+    public SaveNewUserInFile() {
+        if(!getUsersInfoArrayList().isEmpty()) {
+            lastID = getUsersInfoArrayList().size();
+        }
+        else {
+            lastID = 0;
+        }
     }
 
 
-
-
-@Override
-    public void saveNewUser(CalculatorUser calculatorUser){
+    @Override
+    public void saveNewUser(CalculatorUser calculatorUser) {
         lastID++;
         calculatorUser.setUserID(lastID);
 
 
-        try(FileWriter fileWriter = new FileWriter(filePath, true)) {
+        try (FileWriter fileWriter = new FileWriter(filePath, true)) {
 
-                fileWriter.write(calculatorUser.getUserID() + " " + calculatorUser.getUserName() + " " + calculatorUser.getUserEmail() +
-                        " " + calculatorUser.getUserPassword() + " " + calculatorUser.getFormatDateTime());
-                fileWriter.write('\n');
+            fileWriter.write(calculatorUser.getUserID() + " " + calculatorUser.getUserName() + " " + calculatorUser.getUserEmail() +
+                    " " + calculatorUser.getUserPassword() + " " + calculatorUser.getFormatDateTime());
+            fileWriter.write('\n');
 
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -50,27 +54,33 @@ public class SaveNewUserInFile implements SaveNewUser{
     public List<CalculatorUser> getUsersInfoArrayList() {
 
         List<CalculatorUser> usersInfoFromFileToList = new ArrayList<>();
+//        List<CalculatorUser> Optional = new ArrayList<>();
+        if (!usersInfoFromFileToList.isEmpty()) {
+            try {
+                List<String> list = Files.readAllLines(
+                        new File(filePath).toPath());
 
-        try{
-            List <String> list = Files.readAllLines(
-                    new File(filePath).toPath());
+                list.stream().forEach(s -> {
+                    String[] fields = s.split(" ");
+                    CalculatorUser calculatorUser = new CalculatorUser();
+                    calculatorUser.setUserID(Integer.valueOf(fields[0]));
+                    calculatorUser.setUserName(fields[1]);
+                    calculatorUser.setUserEmail(fields[2]);
+                    calculatorUser.setUserPassword(fields[3]);
+                    calculatorUser.setFormatDateTime(fields[4]);
+                    usersInfoFromFileToList.add(calculatorUser);
+                });
 
-            list.stream().forEach(s -> {
-                String[] fields = s.split(" ");
-                CalculatorUser calculatorUser = new CalculatorUser();
-                calculatorUser.setUserID(Integer.valueOf(fields[0]));
-                calculatorUser.setUserName(fields[1]);
-                calculatorUser.setUserEmail(fields[2]);
-                calculatorUser.setUserPassword(fields[3]);
-                calculatorUser.setFormatDateTime(fields[4]);
-                usersInfoFromFileToList.add(calculatorUser);
-            });
-
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return usersInfoFromFileToList;
         }
-        catch (IOException e){
-            e.printStackTrace();
-        }
-        return usersInfoFromFileToList;
+//        else return Optional;
+        else return null;
+
+    }
     }
 
-}
+
+
